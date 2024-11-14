6e83bee5d136d24f27af74360797fcbfeef59d00
Tlilani Ahmed Abdelilah <ahmed.tlilani.8@gmail.com>
2024-11-14 17:33:41 +0100

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,21 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+   float taille_en_bits = w * h * 3 * 8;
 
-   // YOUR CODE HERE - END
-   return 0;
+   if (strcmp(unit, "bt") == 0) {
+       return taille_en_bits;
+   } 
+   else if (strcmp(unit, "ko") == 0) {
+       return taille_en_bits / 1024;
+   } 
+   else if (strcmp(unit, "mo") == 0) {
+       return taille_en_bits / 1024 / 1024;
+   } 
+   else if (strcmp(unit, "go") == 0) {
+       return taille_en_bits / 1024 / 1024 / 1024;
+   } 
+   else {
+       return taille_en_bits;
+   }
 }
