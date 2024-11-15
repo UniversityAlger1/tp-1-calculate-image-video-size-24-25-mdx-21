ea6bd46b814da9fc8d317b21f10c64ff40fc9c4f
Tlilani Ahmed Abdelilah <ahmed.tlilani.8@gmail.com>
2024-11-15 20:34:42 +0100

    Update video.c

--- video.c
+++ video.c
@@ -9,25 +9,19 @@
 // Return value
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-   // YOUR CODE HERE - BEGIN
-    const int bitTo_octet = 8;
-    const int bitTo_kilobits = 1024;
-    const int bitTo_megabytes = 1024 * 1024;
-    const int bitTo_gigabytes = 1024 * 1024 * 1024;
+    float colorSectionSize = w * h * 3 * fps * durationMovie; // 3 bytes per pixel for RGB color
+    float bwSectionSize = w * h * 1 * fps * durationCredits; // 1 byte per pixel for grayscale
 
-    long long tailleSectionCouleur = (long long)w * h * 24 * fps * durationMovie;
-    long long tailleSectionNB = (long long)w * h * 8 * fps * durationCredits;
-    long long taille_en_bits = tailleSectionCouleur + tailleSectionNB;
-    float tailleEnOctets = taille_en_bits / (float)bitTo_octet;
+    float totalSizeBytes = colorSectionSize + bwSectionSize;
 
     if (strcmp(unit, "bt") == 0) {
-        return taille_en_bits;
+        return totalSizeBytes;
     } else if (strcmp(unit, "ko") == 0) {
-        return taille_en_bits / bitTo_kilobits;
+        return totalSizeBytes / 1024;
     } else if (strcmp(unit, "mo") == 0) {
-        return tailleEnOctets / (1024 * 1024);
+        return totalSizeBytes / (1024 * 1024);
     } else if (strcmp(unit, "go") == 0) {
-        return tailleEnOctets / (1024 * 1024 * 1024);
+        return totalSizeBytes / (1024 * 1024 * 1024);
     } else {
         return 0;
     }
