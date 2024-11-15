5fc3ae0a1a3cfc6c845bc6ead609e893dc84127d
Tlilani Ahmed Abdelilah <ahmed.tlilani.8@gmail.com>
2024-11-15 11:55:26 +0100

    Update video.c

--- video.c
+++ video.c
@@ -11,25 +11,23 @@
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
     const int bitTo_octet = 8;
-    const int bitTo_kilooctet = 1024;
-    const int bitTo_megaoctet = 1024;
-    const int bitTo_gigaoctet = 1024;
+    const int bitTo_kilobits = 1024;
+    const int bitTo_megabytes = 1024 * 1024;
+    const int bitTo_gigabytes = 1024 * 1024 * 1024;
 
     long long tailleSectionCouleur = (long long)w * h * 24 * fps * durationMovie;
     long long tailleSectionNB = (long long)w * h * 8 * fps * durationCredits;
-
     long long taille_en_bits = tailleSectionCouleur + tailleSectionNB;
-
     float tailleEnOctets = taille_en_bits / (float)bitTo_octet;
 
     if (strcmp(unit, "bt") == 0) {
         return taille_en_bits;
     } else if (strcmp(unit, "ko") == 0) {
-        return tailleEnOctets / bitTo_kilooctet;
+        return taille_en_bits / bitTo_kilobits;
     } else if (strcmp(unit, "mo") == 0) {
-        return tailleEnOctets / (bitTo_kilooctet * bitTo_megaoctet);
+        return tailleEnOctets / (1024 * 1024);
     } else if (strcmp(unit, "go") == 0) {
-        return tailleEnOctets / (bitTo_kilooctet * bitTo_megaoctet * bitTo_gigaoctet);
+        return tailleEnOctets / (1024 * 1024 * 1024);
     } else {
         return 0;
     }
